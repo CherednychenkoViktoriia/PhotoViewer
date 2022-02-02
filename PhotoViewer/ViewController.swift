4ad1d4216b16551b98f5d74b960746bc80a9db27
Viktoriia Cherednychenko <viktoryacherednichenko@gmail.com>
2022-02-02 23:38:35 +0200

    Initial Commit

---
+++ PhotoViewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoViewer
+//
+//  Created by Viktoriia Cherednychenko on 02.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
